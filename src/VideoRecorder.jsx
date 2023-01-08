4c1e8c3cb5fc48089ea83ba604a06b1800220016
Iniubong Obonguko <iniubongobonguko2018@gmail.com>
2023-01-08 02:40:34 +0100

    finish application

--- src/VideoRecorder.jsx
+++ src/VideoRecorder.jsx
@@ -1,45 +1,133 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
+
+const mimeType = 'video/webm; codecs="opus,vp8"';
 
 const VideoRecorder = () => {
 	const [permission, setPermission] = useState(false);
+
+	const mediaRecorder = useRef(null);
+
+	const liveVideoFeed = useRef(null);
+
+	const [recordingStatus, setRecordingStatus] = useState("inactive");
+
 	const [stream, setStream] = useState(null);
 
+	const [recordedVideo, setRecordedVideo] = useState(null);
+
+	const [videoChunks, setVideoChunks] = useState([]);
+
 	const getCameraPermission = async () => {
+		setRecordedVideo(null);
+		//get video and audio permissions and then stream the result media stream to the videoSrc variable
 		if ("MediaRecorder" in window) {
 			try {
-				const streamData = await navigator.mediaDevices.getUserMedia({
-					audio: true,
+				const videoConstraints = {
+					audio: false,
 					video: true,
-				});
-				setPermission(true);
-				setStream(streamData);
-			} catch {
-				alert(
-					"You denied access to the camera and microphone, so this demo will not work."
+				};
+				const audioConstraints = { audio: true };
+
+				// create audio and video streams separately
+				const audioStream = await navigator.mediaDevices.getUserMedia(
+					audioConstraints
 				);
+				const videoStream = await navigator.mediaDevices.getUserMedia(
+					videoConstraints
+				);
+
+				setPermission(true);
+
+				//combine both audio and video streams
+
+				const combinedStream = new MediaStream([
+					...videoStream.getVideoTracks(),
+					...audioStream.getAudioTracks(),
+				]);
+
+				setStream(combinedStream);
+
+				//set videostream to live feed player
+				liveVideoFeed.current.srcObject = videoStream;
+			} catch (err) {
+				alert(err.message);
 			}
 		} else {
 			alert("The MediaRecorder API is not supported in your browser.");
 		}
 	};
 
+	const startRecording = async () => {
+		setRecordingStatus("recording");
+
+		const media = new MediaRecorder(stream, { mimeType });
+
+		mediaRecorder.current = media;
+
+		mediaRecorder.current.start();
+
+		let localVideoChunks = [];
+
+		mediaRecorder.current.ondataavailable = (event) => {
+			if (typeof event.data === "undefined") return;
+			if (event.data.size === 0) return;
+			localVideoChunks.push(event.data);
+		};
+
+		setVideoChunks(localVideoChunks);
+	};
+
+	const stopRecording = () => {
+		setPermission(false);
+		setRecordingStatus("inactive");
+		mediaRecorder.current.stop();
+
+		mediaRecorder.current.onstop = () => {
+			const videoBlob = new Blob(videoChunks, { type: mimeType });
+			const videoUrl = URL.createObjectURL(videoBlob);
+
+			setRecordedVideo(videoUrl);
+
+			setVideoChunks([]);
+		};
+	};
+
 	return (
 		<div>
 			<h2>Video Recorder</h2>
 			<main>
-				<div className="controls">
-					{!permission && (
+				<div className="video-controls">
+					{!permission ? (
 						<button onClick={getCameraPermission} type="button">
 							Get Camera
 						</button>
-					)}
-					{permission && (
-						<button onClick={getCameraPermission} type="button">
-							Record
+					) : null}
+					{permission && recordingStatus === "inactive" ? (
+						<button onClick={startRecording} type="button">
+							Start Recording
 						</button>
-					)}
+					) : null}
+					{recordingStatus === "recording" ? (
+						<button onClick={stopRecording} type="button">
+							Stop Recording
+						</button>
+					) : null}
 				</div>
 			</main>
+
+			<div className="video-player">
+				{!recordedVideo ? (
+					<video ref={liveVideoFeed} autoPlay className="live-player"></video>
+				) : null}
+				{recordedVideo ? (
+					<div className="recorded-player">
+						<video className="recorded" src={recordedVideo} controls></video>
+						<a download href={recordedVideo}>
+							Download Recording
+						</a>
+					</div>
+				) : null}
+			</div>
 		</div>
 	);
 };
